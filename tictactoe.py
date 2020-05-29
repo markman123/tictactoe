fdf12dae7271a9e94eb6705729086faa5feaaaa9
Mark Marlow <MarloMa@cba.com.au>
2020-05-30 08:19:43 +1000

    added player modes

--- tictactoe.py
+++ tictactoe.py
@@ -1,47 +1,46 @@
 from enum import Enum
 from os import system
 
+class GameMode(Enum):
+    ONE_PLAYER = 0
+    TWO_PLAYER = 1
 
-class States(Enum):
+class GameState(Enum):
+    PLAYING = 0
+    EXIT = 1
+    GAME_OVER = 2
+
+class BoardState(Enum):
     BLANK = 0
     X = 1
     O = 2
 
     def __str__(self):
         if self.name == "BLANK":
-            return " - "
+            return "-"
         elif self.name == "X":
-            return " X "
+            return "X"
         elif self.name == "O":
-            return " O "
+            return "O"
 
 
-"""
-+---+----+---+
-| - | - | - |
-| - | - | - |
-| - | - | - |
-"""
-"""
-+---+----+---+
-| 00 | 01 | 02 |
-| 10 | 11 | 12 |
-| 20 | 21 | 22 |
-[00 11 22][02 11 20]
-"""
+
 
 
 class TicTacToe:
     def __init__(self):
-        self.board = [[States.BLANK for _ in range(3)] for _ in range(3)]
+        self.board = [[BoardState.BLANK for _ in range(3)] for _ in range(3)]
 
     def __repr__(self):
-        output = "   +-0-+-1-+-2-+\n"
+        output = "   +-0--+-1--+-2--+\n"
         for idx, row in enumerate(self.board):
-            row = [str(i) for i in row]
-            row = f"{idx}: |{row[0]}|{row[1]}|{row[2]}|"
+            
+            cell0 = f" {idx}0 " if row[0] == BoardState.BLANK else f" {row[0]}  "
+            cell1 = f" {idx}1 " if row[1] == BoardState.BLANK else f" {row[1]}  "
+            cell2 = f" {idx}2 " if row[2] == BoardState.BLANK else f" {row[2]}  "
+            row = f"{idx}: |{cell0}|{cell1}|{cell2}|"
             output += row + "\n"
-        output += "   +---+---+---+"
+        output += "   +----+----+----+"
         return output
 
     def get(self, x, y=None):
@@ -50,49 +49,88 @@ class TicTacToe:
 
         return self.board[x][y]
 
-    def set(self, x, y, to: States):
+    def set(self, x, y, to: BoardState):
         self.board[x][y] = to
 
     def print(self):
         print(str(self))
 
+    def print_guide(self):
+        print("""
+   +--0-+--1-+--2-+
+0: | 00 | 01 | 02 |
+1: | 10 | 11 | 12 |
+2: | 20 | 21 | 22 |
+   +----+----+----+
+""")
+
+
+class GameAI():
+    scoring = {BoardState.O: 1, BoardState.X = -1}
+    def __init__(self):
+        self.ai = BoardState.O
+    
 
 class Game:
     def __init__(self):
         self.board = TicTacToe()
         self.turns = []
-        self.whos_turn = States.X
+        self.whos_turn = BoardState.X
+        self.game_mode = None
+
+    def select_game_mode(self):
+    
+        print("[1]    vs. AI")
+        print("[2]    2 Player")
+        print("[Else] Quit")
+        game_mode = input("Select game mode: ")
+
+        if game_mode.lower() not in ["1", "2"]:
+            self.exit_game()
+
+        if game_mode == "1":
+            self.game_mode = GameMode.ONE_PLAYER
+            print("Playing against me! ... you're gonna lose. Loser.")
+        elif game_mode == "2":
+            self.game_mode = GameMode.TWO_PLAYER
+            print("Playing with a friend... (BS! Against yourself)")
+
+
 
     def start_game(self):
+        self.select_game_mode()
         self.game_loop()
+
+    def exit_game(self):
+        print("Please, play again! I'm lonely...")
+        exit(0)
 
     def game_loop(self):
         system("cls")
         while True:
             print("Here is the board:")
-            print(self.board)
+            self.board.print()
             turn = input(
-                f"[Turn of {str(self.whos_turn)}] Enter turn as colrow e.g. 01, 22 (q to quit): "
+                f"[Turn of {str(self.whos_turn)}] Enter coords (q to quit): "
             )
             turn = self.parse_turn(turn)
-            if turn == -1:
-                print("Please, play again! I'm lonely...")
-                exit(0)
-            elif turn:
+            if turn == GameState.EXIT:
+                self.exit_game()
+            elif not isinstance(turn, GameState):
                 self.process_turn(turn[0], turn[1])
 
     @staticmethod
     def _check_all(check):
-        x = lambda x: x == States.X
-        o = lambda x: x == States.O
+        x = lambda x: x == BoardState.X
+        o = lambda x: x == BoardState.O
         x_winner = all([x(i) for i in check])
         o_winner = all([o(i) for i in check])
 
         if x_winner:
-            return States.X
+            return BoardState.X
 
         elif o_winner:
-            return States.O
+            return BoardState.O
 
     def check_winner(self):
         # Check horizontals
@@ -120,7 +158,9 @@ class Game:
 
         # Check stale mate
         if self._is_stale_mate():
-            return -1
+            return GameState.GAME_OVER
+        
+        return GameState.PLAYING
 
     def _is_stale_mate(self):
         flat = []
@@ -128,38 +168,42 @@ class Game:
             for y in range(3):
                 flat.append(self.board.get(x, y))
 
-        not_blank = lambda x: x != States.BLANK
+        not_blank = lambda x: x != BoardState.BLANK
 
         return all([not_blank(i) for i in flat])
 
     def reset_game(self):
         self.__init__()
+        self.start_game()
 
     def play_again(self):
         play_again = input("Play again? Y/N: ")
-        if play_again.lower() == "Y":
+        if play_again.lower() == "y":
             self.reset_game()
         else:
-            exit(0)
+            self.exit_game()
 
     def process_turn(self, x, y):
         self.turns.append((x, y))
         self.board.set(x, y, self.whos_turn)
         winner = self.check_winner()
-        if winner and winner.value >= 0:
+        if isinstance(winner, BoardState):
             system("cls")
-            print(self.board)
+            self.board.print()
             print(f"Congratulations player {str(winner)}! You've won!!")
+            self.play_again()
+        elif winner == GameState.GAME_OVER:
+            print("Yawn... stale mate")
             self.play_again()
         else:
             system("cls")
             self.switch_player()
 
     def switch_player(self):
-        if self.whos_turn == States.X:
-            self.whos_turn = States.O
+        if self.whos_turn == BoardState.X:
+            self.whos_turn = BoardState.O
         else:
-            self.whos_turn = States.X
+            self.whos_turn = BoardState.X
 
     @staticmethod
     def error_text(input):
@@ -168,24 +212,24 @@ class Game:
 
     def parse_turn(self, turn):
         if turn == "q":
-            return -1
+            return GameState.EXIT
         if len(turn) != 2:
             Game.error_text(turn)
-            return
+            return GameState.PLAYING
 
         if not turn[0].isdigit() or not turn[1].isdigit():
             Game.error_text(turn)
-            return
+            return GameState.PLAYING
 
         x, y = int(turn[0]), int(turn[1])
 
         if x > 2 or y > 2 or x < 0 or y < 0:
             Game.error_text(turn)
-            return
+            return GameState.PLAYING
 
-        if self.board.get(x, y) != States.BLANK:
+        if self.board.get(x, y) != BoardState.BLANK:
             Game.error_text(f"\nNot a blank space! {turn}")
-            return
+            return GameState.PLAYING
 
         return x, y
 
