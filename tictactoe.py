74ddf69c0d12733857f0457796ab8fec1c21712c
Mark Marlow <MarloMa@cba.com.au>
2020-05-30 08:22:18 +1000

    factored out the enums

--- tictactoe.py
+++ tictactoe.py
@@ -1,31 +1,5 @@
-from enum import Enum
+from enums import GameMode, GameState, BoardState
 from os import system
-
-class GameMode(Enum):
-    ONE_PLAYER = 0
-    TWO_PLAYER = 1
-
-class GameState(Enum):
-    PLAYING = 0
-    EXIT = 1
-    GAME_OVER = 2
-
-class BoardState(Enum):
-    BLANK = 0
-    X = 1
-    O = 2
-
-    def __str__(self):
-        if self.name == "BLANK":
-            return "-"
-        elif self.name == "X":
-            return "X"
-        elif self.name == "O":
-            return "O"
-
-
-
-
 
 class TicTacToe:
     def __init__(self):
